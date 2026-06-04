cf1d113a21f496eab6ff0cf0a0d32030c04b95a0
Adam Shreim <shreimadambusi@gmail.com>
2026-06-04 11:53:03 +0300

    Adjustment on home page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,9 @@
 export default function Dashboard() {
   return (
-<div className="p-8">
-      <div className="w-[600px]">
-        <p className="text-left">
-          So I started to walk into the water. I won't lie to you boys, I was
-          terrified. But I pressed on, and as I made my way past the breakers a
-          strange calm came over me. I don't know if it was divine intervention
-          or the kinship of all living things but I tell you
-        </p>
+    <div className="">
+      <div className="w-1/2">
+        <p>Dashboard</p>
       </div>
-  </div>
+    </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,27 +4,29 @@ import "../styles/Home.css";
 
 function Hero() {
   return (
-    <section className=" w-[80%] mx-auto min-h-screen flex items-center justify-between">
+    <section className=" w-[80%] mx-auto min-h-screen flex items-center justify-between gap-16">
       {/* left side */}
       <div className="w-1/2 flex justify-start text-left">
         <div className="max-w-xl">
-          <p className="inline-block px-4 py-2 mb-6 rounded-full bg-blue-100 text-blue-700 font-medium">
-            AI Resume Analyzer
-          </p>
+          <div className="items-center">
+            <p className="ml-8 px-4 py-2 mb-6 rounded-full bg-blue-100 text-blue-700 font-medium text-5xl">
+              AI Resume Analyzer
+            </p>
+          </div>
           <div className="t">
-            <h1 className="text-4xl font-bold leading-tight mb-6">
+            <h1 className="text-4xl md:text-5xl font-bold leading-tight mb-6 ml-8">
               Analyze, Improve and Optimize Your Resume with AI
             </h1>
-            <p className="text-gray-600 text-lg mb-8">
+            <p className="text-gray-600 text-lg mb-8 ml-8">
               Get instant feedback on your resume, improve ATS compatibility,
               and identify areas that can strengthen your job applications.
             </p>
           </div>
-          <div className="flex gap-6">
-            <button className="bg-blue-600 rounded-xl p-3 text-white shadow-lg hover:bg-blue-700">
+          <div className="flex gap-6 justify-center ml-8">
+            <button className="bg-blue-600 rounded-xl px-6 py-3 text-white shadow-lg hover:bg-blue-500 transition font-medium">
               Get Started
             </button>
-            <button className="p-3 bg-gray-300 rounded-xl shadow-lg hover:bg-gray-400">
+            <button className="px-6 py-3 bg-gray-100 font-medium rounded-xl shadow-lg hover:bg-gray-200 transition">
               learn More
             </button>
           </div>
@@ -32,17 +34,18 @@ function Hero() {
       </div>
 
       {/* right side*/}
-      <div className="flex justify-center w-1/2">
+      <div className="flex justify-center w-1/2 pl-10">
         <div className="relative">
           {/* ATS Card*/}
-          <div className="absolute -top-8 right-0 bg-white rounded-xl p-4 shadow-lg">
+          <div className="absolute -top-8 -right-3 bg-gray-50 rounded-xl p-4 shadow-lg px-5 py-3 z-10 transition duration-300 hover:-translate-y-1">
             {" "}
             ATS Friendly ✓
           </div>
           {/* Main card*/}
-          <div className="bg-white rounded-xl p-4 shadow-lg w-80">
+          <div className="bg-white rounded-xl p-8 shadow-2xl w-80 transition duration-300 hover:-translate-y-1">
             <h3 className="text-xl font-bold mb-4">Resume Score</h3>
-            <p className="text-2xl font-bold mb-6">89/100</p>
+            <p className="text-2xl font-bold mb-1 text-blue-500">89</p>
+            <p className="text-gray-500 mt-1 mb-6 text-xl">out of 100</p>
             <h4 className="font-semibold mb-2">Skills Found</h4>
             <ul>
               <li>✓ React</li>
@@ -51,7 +54,7 @@ function Hero() {
             </ul>
           </div>
           {/* Skills Card*/}
-          <div className="absolute -bottom-6 right-10 bg-white rounded-xl p-4 shadow-lg">
+          <div className="absolute -bottom-6 right-10 bg-gray-100 rounded-xl px-5 py-3 shadow-lg transition duration-300 hover:-translate-y-1">
             React ✓
           </div>
         </div>
